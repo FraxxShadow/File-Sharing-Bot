1ea315f7932e4df1208999fdf71ea5dc902b9be8
FraxxShadow <jujutsukaigojosaturo@gmail.com>
2025-04-24 08:25:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -191,7 +191,7 @@ async def not_joined(client: Client, message: Message):
 
     await message.reply_photo(
         photo=FORCE_PIC,
-        text=FORCE_MSG.format(
+        caption=FORCE_MSG.format(
             first=message.from_user.first_name,
             last=message.from_user.last_name,
             username=None if not message.from_user.username else '@' + message.from_user.username,
